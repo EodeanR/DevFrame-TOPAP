a4c53bc69ec531d017a6812b7a6a54da226d2bb3
EodeanR <irama141102@gmail.com>
2023-03-16 01:42:25 +0700

    add template

---
+++ resources/views/template/navbar.blade.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <h1 class="h3">TOPAP</h1>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" href="{{ url('/')}}" aria-current="page">Home</a>
+                    <a class="nav-link" href="{{ url('games') }}">Games</a>
+                    <a class="nav-link" href="#">Pulsa</a>
+                    <a class="nav-link" href="#">Voucher</a>
+                    <a class="nav-link" href="#">Joki</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
