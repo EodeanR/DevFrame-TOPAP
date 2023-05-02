2a1b71211e6e68b93c46f0510fd376639ef76ddf
EodeanR <irama141102@gmail.com>
2023-05-03 00:28:51 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('template/header') . view('template/navbar') . view('pages/home') . view('template/footer');
+    $data = [
+        'title' => 'HOME - TOPAP'
+    ];
+    return view('template/header', $data) . view('template/navbar') . view('pages/home') . view('template/footer');
 });
 Route::get('/games', function () {
-    return view('template/header') . view('template/navbar') . view('pages/games') . view('template/footer');
+    $data = [
+        'title' => 'GAMES - TOPAP'
+    ];
+    return view('template/header', $data) . view('template/navbar') . view('pages/games') . view('template/footer');
+});
+Route::get('/pulsa', function () {
+    $data = [
+        'title' => 'PULSA - TOPAP'
+    ];
+    return view('template/header', $data) . view('template/navbar') . view('pages/pulsa') . view('template/footer');
+});
+Route::get('/voucher', function () {
+    $data = [
+        'title' => 'VOUCHER - TOPAP'
+    ];
+    return view('template/header', $data) . view('template/navbar') . view('pages/voucher') . view('template/footer');
+});
+Route::get('/joki', function () {
+    $data = [
+        'title' => 'JOKI - TOPAP'
+    ];
+    return view('template/header', $data) . view('template/navbar') . view('pages/joki') . view('template/footer');
+});
+Route::get('/pricelist', function () {
+    $data = [
+        'title' => 'PRICE LIST - TOPAP'
+    ];
+    return view('template/header', $data) . view('template/navbar') . view('pages/pricelist') . view('template/footer');
+});
+Route::get('/backdoor-admin', function () {
+    $data = [
+        'title' => 'DASHBOARD ADMIN - TOPAP'
+    ];
+    return view('template/header', $data) . view('admin/backdoor-admin') . view('template/footer');
 });
