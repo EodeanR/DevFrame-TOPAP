14444c9c7c3e7e94cf928a79498a4fa99163b4c7
EodeanR <irama141102@gmail.com>
2023-05-07 20:49:32 +0700

    update partials user

---
+++ resources/views/_partials/footer.blade.php
@@ -0,0 +1,13 @@
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+</script>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+
+</html>
