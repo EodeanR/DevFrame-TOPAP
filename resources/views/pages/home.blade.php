840d866f2c0c018257b9c3cf6edbf06c25e88ab5
EodeanR <irama141102@gmail.com>
2023-05-10 14:01:32 +0700

    home update

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,3 +1,53 @@
-<div class="container">
-    <h1>HOME</h1>
+<!-- <h1>HOME</h1> -->
+<div class="special-event-slide mb-5">
+    <div id="carouselAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://sin1.contabostorage.com/0a986eb902c4469cb860e43985eb18a1:vocapanel/muhabenostore/banner ramadhanaa-c1c2-original.webp" class="d-block w-100" alt="...">
+
+            </div>
+            <div class="carousel-item">
+                <img src="https://sin1.contabostorage.com/0a986eb902c4469cb860e43985eb18a1:vocapanel/muhabenostore/banner ramadhanaa-c1c2-original.webp" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://sin1.contabostorage.com/0a986eb902c4469cb860e43985eb18a1:vocapanel/muhabenostore/banner ramadhanaa-c1c2-original.webp" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselAutoplaying" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselAutoplaying" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+
+<!-- <div class="search justify-content-end mt-4 mt-md-0">
+            <form class="d-flex" role="search">
+                <input class="form-control bg-dark text-secondary rounded-0 border-primary" type="search"
+                    placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-warning rounded-0" type="submit"><i
+                        class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </div> -->
+
+
+<div class="content-games">
+    <h2 class="mb-3 text-primary">Games</h2>
+    <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 row-cols-xl-6">
+        <div class="col mb-2">
+            <a href="" class="nav-link">
+                <div class="card">
+                    <img src="assets/img/mole.jpg" class="card-img-top" alt="Mobile Legend" />
+                    <div class="card-body">
+                        <h6 class="card-title link">Mobile Legend</h6>
+                        <small class="card-text text-muted">Moonton</small>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+    </div>
 </div>
